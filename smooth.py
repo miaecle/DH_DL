f284074336ddd8a24234f3c96c696cb3dc166838
Zhenqin Wu <zhenqin.wu2@gmail.com>
2021-01-14 19:57:05 +0800

    Update smooth.py

--- smooth.py
+++ smooth.py
@@ -1,14 +1,45 @@
 import os
 import numpy as np
 import csv
-
+import torch
 
 def euclidean_similarity(si, sj):
-    return -np.linalg.norm(si - sj, ord=2, axis=-1)
+    return -np.linalg.norm(si - sj, ord=2, axis=2)
 
 
 def cosine_similarity(si, sj):
     return (si * sj).sum(2) / (np.sqrt((si**2).sum(2)) * np.sqrt((sj**2).sum(2)))
+
+
+def calculate_block_similarity(si, sj, sim_fn=euclidean_similarity):
+
+    assert si.shape[1] == 1
+    assert sj.shape[0] == 1
+    n_dim = si.shape[-1]
+    
+    batch_size = int(np.floor(1e6 / n_dim))
+    if batch_size > sj.shape[1]:
+        j_batch_size = sj.shape[1]
+        i_batch_size = batch_size // j_batch_size
+    else:
+        j_batch_size = batch_size
+        i_batch_size = 1
+
+    n_i_batches = int(np.ceil(si.shape[0] / i_batch_size))
+    n_j_batches = int(np.ceil(sj.shape[1] / j_batch_size))
+
+    total_combined = []
+    for i in range(n_i_batches):
+        row_combined = []
+        for j in range(n_j_batches):
+            _si = si[(i*i_batch_size):((i+1)*i_batch_size)]
+            _sj = sj[:, (j*j_batch_size):((j+1)*j_batch_size)]
+            block_sim = sim_fn(_si, _sj)
+            row_combined.append(block_sim)
+        row_combined = np.concatenate(row_combined, 1)
+        total_combined.append(row_combined)
+    total_combined = np.concatenate(total_combined, 0)
+    return total_combined
 
 
 def find_kNN(samples, pool, k=20, sim_fn=euclidean_similarity):
@@ -16,16 +47,11 @@ def find_kNN(samples, pool, k=20, sim_fn=euclidean_similarity):
     assert samples.shape[1] == pool.shape[1]
     assert pool.shape[0] > k
     n_dim = samples.shape[1]
-
-    batch_size = int(np.floor(10000 / n_dim))
-    n_batches = int(np.ceil(samples.shape[0] / batch_size))
-
-    kNN_inds = []
-    for i in range(n_batches):
-        batch = samples[(i*batch_size):((i+1)*batch_size)]
-        batch_sim = sim_fn(batch.reshape((-1, 1, n_dim)), pool.reshape((1, -1, n_dim)))[0]
-        batch_kNNs = [np.argsort(row_sim)[-k:] for row_sim in batch_sim]
-        kNN_inds.extend(batch_kNNs)
+    
+    sim_mat = calculate_block_similarity(samples.reshape((-1, 1, n_dim)), 
+                                         pool.reshape((1, -1, n_dim)), 
+                                         sim_fn=sim_fn)
+    kNN_inds = [np.argsort(row_sim)[-k:] for row_sim in sim_mat]
     return np.array(kNN_inds)
 
 
@@ -41,8 +67,8 @@ def smooth_test_with_train(test_Xs,
     test_embedding = None
     train_embedding = None
     if model is not None:
-        test_inputs = t.from_numpy(test_Xs).float()
-        train_inputs = t.from_numpy(train_Xs).float()
+        test_inputs = torch.from_numpy(test_Xs).float()
+        train_inputs = torch.from_numpy(train_Xs).float()
         if model.gpu:
             test_inputs = test_inputs.cuda()
             train_inputs = train_inputs.cuda()
